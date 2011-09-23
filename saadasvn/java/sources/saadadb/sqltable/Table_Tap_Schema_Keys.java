770553c245bac65c61ed576473b185694f3c110e
laurent.mistahl <laurent.mistahl@ff1e8092-12d3-63eb-d73d-7507d0dcd924>
2011-09-23 13:28:46 +0000

    TAP implementation (start)

--- saadasvn/java/sources/saadadb/sqltable/Table_Tap_Schema_Keys.java
+++ saadasvn/java/sources/saadadb/sqltable/Table_Tap_Schema_Keys.java
@@ -3,10 +3,47 @@
  */
 package saadadb.sqltable;
 
+import java.util.LinkedHashMap;
+
+import saadadb.exceptions.SaadaException;
+import saadadb.meta.AttributeHandler;
+
 /**
  * @author laurent
  * @version $Id$
  */
 public class Table_Tap_Schema_Keys extends SQLTable {
+	public static final LinkedHashMap<String, AttributeHandler> attMap;
+
+	static {
+		attMap = new LinkedHashMap<String, AttributeHandler>();
+		AttributeHandler ah;
+		ah = new AttributeHandler();
+		ah.setNameattr("key_id"); ah.setNameattr("key_id"); ah.setType("VARCHAR"); ah.setComment("unique key identifie");
+		attMap.put("key_id", ah);
+		ah = new AttributeHandler();
+		ah.setNameattr("from_table"); ah.setNameattr("from_table"); ah.setType("VARCHAR"); ah.setComment("fully qualified table name");
+		attMap.put("from_table", ah);
+		ah = new AttributeHandler();
+		ah.setNameattr("target_table"); ah.setNameattr("target_table"); ah.setType("VARCHAR"); ah.setComment("fully qualified table name");
+		attMap.put("target_table", ah);
+		ah = new AttributeHandler();
+		ah.setNameattr("description"); ah.setNameattr("description"); ah.setType("VARCHAR"); ah.setComment("description of this key");
+		attMap.put("description", ah);
+		ah = new AttributeHandler();
+		ah.setNameattr("utype"); ah.setNameattr("utype"); ah.setType("VARCHAR"); ah.setComment("utype of this key");
+		attMap.put("utype", ah);
+	}
+	/**
+	 * @throws SaadaException
+	 */
+	public static  void createTable() throws SaadaException {
+		String sql = "";
+		for (AttributeHandler ah: attMap.values() ) {
+			if( sql.length() > 0 ) sql += ", ";
+			sql += ah.getNameattr() + "  " + ah.getType();
+		}
+		SQLTable.createTable("tap_schema_keys", sql, null, false);
+	}
 
 }
--- saadasvn/java/sources/saadadb/sqltable/Table_Tap_Schema_Tables.java
+++ saadasvn/java/sources/saadadb/sqltable/Table_Tap_Schema_Tables.java
@@ -1,10 +1,47 @@
 package saadadb.sqltable;
 
+import java.util.LinkedHashMap;
+
+import saadadb.exceptions.SaadaException;
+import saadadb.meta.AttributeHandler;
+
 /**
  * @author laurent
  * @version $Id@
  *
  */
 public class Table_Tap_Schema_Tables extends SQLTable {
+	public static final LinkedHashMap<String, AttributeHandler> attMap;
+
+	static {
+		attMap = new LinkedHashMap<String, AttributeHandler>();
+		AttributeHandler ah;
+		ah = new AttributeHandler();
+		ah.setNameattr("schema_name"); ah.setNameattr("schema_name"); ah.setType("VARCHAR"); ah.setComment("the schema name from TAP_SCHEMA.schemas");
+		attMap.put("schema_name", ah);
+		ah = new AttributeHandler();
+		ah.setNameattr("table_name"); ah.setNameattr("table_name"); ah.setType("VARCHAR"); ah.setComment("table name as it should be used in queries");
+		attMap.put("table_name", ah);
+		ah = new AttributeHandler();
+		ah.setNameattr("table_type"); ah.setNameattr("table_type"); ah.setType("VARCHAR"); ah.setComment("one of: table, view");
+		attMap.put("table_type", ah);
+		ah = new AttributeHandler();
+		ah.setNameattr("description"); ah.setNameattr("description"); ah.setType("VARCHAR"); ah.setComment("brief description of table");
+		attMap.put("description", ah);
+		ah = new AttributeHandler();
+		ah.setNameattr("utype"); ah.setNameattr("utype"); ah.setType("VARCHAR"); ah.setComment("UTYPE if table corresponds to a data model");
+		attMap.put("utype", ah);
+		}
+	/**
+	 * @throws SaadaException
+	 */
+	public static  void createTable() throws SaadaException {
+		String sql = "";
+		for (AttributeHandler ah: attMap.values() ) {
+			if( sql.length() > 0 ) sql += ", ";
+			sql += ah.getNameattr() + "  " + ah.getType();
+		}
+		SQLTable.createTable("tap_schema_tables", sql, null, false);
+	}
 
 }
