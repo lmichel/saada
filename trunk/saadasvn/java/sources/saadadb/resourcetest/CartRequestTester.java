358435598532b6083e825a43812e6be179b3ec70
laurent.mistahl <laurent.mistahl@ff1e8092-12d3-63eb-d73d-7507d0dcd924>
2011-12-07 09:15:51 +0000

    Shopping cart feature

---
+++ trunk/saadasvn/java/sources/saadadb/resourcetest/CartRequestTester.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package saadadb.resourcetest;
+
+import java.util.LinkedHashMap;
+
+import saadadb.database.Database;
+import saadadb.util.Messenger;
+import saadadb.vo.request.CartRequest;
+
+/**
+ * @author laurent
+ * @version $Id$
+ */
+public class CartRequestTester {
+
+	/**
+	 * @param args
+	 * @throws Exception 
+	 */
+	public static void main(String[] args) throws Exception {
+		Messenger.debug_mode = false;
+		Database.init(args[args.length - 1]);
+		
+		LinkedHashMap<String, String> pmap = new LinkedHashMap<String, String>();
+		String jsonRequest = "{" 
+		+ "\"SpectroscopicSample_SPECTRUM\":{\""
+		+ "queries\":["
+		+ "{\"name\":\"XBSEntry\",\"uri\":\"Select ENTRY From XBSEntry In SpectroscopicSample\", \"relations\": [\"any-relations\"]}"
+		+ "]"
+		+",\"files\":["
+		+ "{\"name\":\"XBSJ000532.7+200716_osp.imh\",\"uri\":\"1154047670801661979\", \"relations\": [\"any-relations\"]}"
+		+ "]"
+		+ "}"
+		+ "}";
+		pmap.put("cart", jsonRequest);
+		
+		CartRequest request = new CartRequest("NoSession", "/home/michel/Desktop/CART");
+		request.addFormator("zip");
+		request.setResponseFilePath("ZippedSaadaql");
+		request.processRequest(pmap);
+		
+	}
+
+}
