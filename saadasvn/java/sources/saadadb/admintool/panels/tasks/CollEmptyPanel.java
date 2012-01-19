b15e6a0bfa8c3db36bbb452aec22169102e22bbe
laurent.mistahl <laurent.mistahl@ff1e8092-12d3-63eb-d73d-7507d0dcd924>
2012-01-19 08:14:24 +0000

    refactoring

---
+++ saadasvn/java/sources/saadadb/admintool/panels/tasks/CollEmptyPanel.java
@@ -0,0 +1,17 @@
+package saadadb.admintool.panels.tasks;
+
+import saadadb.admintool.AdminTool;
+import saadadb.admintool.cmdthread.ThreadDropCollection;
+import saadadb.admintool.cmdthread.ThreadEmptyCollection;
+
+public class CollEmptyPanel extends CollDropPanel {
+
+	public CollEmptyPanel(AdminTool rootFrame, String ancestor) {
+		super(rootFrame, EMPTY_COLLECTION, new ThreadEmptyCollection(rootFrame), ancestor);
+	}
+
+	public void initCmdThread() {
+		cmdThread = new ThreadEmptyCollection(rootFrame);
+	}
+
+}
