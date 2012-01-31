93694fd80221ec7e664327aeb7567a01b2cde1db
laurent.mistahl <laurent.mistahl@ff1e8092-12d3-63eb-d73d-7507d0dcd924>
2012-01-31 15:55:44 +0000

    Admin tool progress

--- saadasvn/java/sources/saadadb/admintool/components/ColorCellRenderer.java
+++ saadasvn/java/sources/saadadb/admintool/components/ColorCellRenderer.java
@@ -16,12 +16,25 @@ class ColorCellRenderer extends DefaultTableCellRenderer {
 	public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected,
 			boolean hasFocus, int row, int column) {
 		super.getTableCellRendererComponent(table,value,isSelected,hasFocus,row,column);
+
 		SQLJTable ct=(SQLJTable)table;
 		if(ct.hasModifiedItem(row) ){
-			this.setBackground(Color.cyan);
+			this.setBackground(Color.orange);
+			ct.modified = true;
 		}
 		else {
-			this.setBackground(Color.white);
+			if(ct.isCellSelected(row, column))  {
+				this.setForeground(Color.WHITE);
+				this.setBackground(Color.GRAY);
+
+			} else {
+				this.setForeground(Color.BLACK);
+				if (row % 2 == 0 ) {
+					this.setBackground(AdminComponent.LIGHTBACKGROUND);
+				} else {
+					this.setBackground(Color.WHITE);
+				}
+			}
 		}
 		return this;
 	}
