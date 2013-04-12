d0febf369815a7e1f403eb9ceaace59950c98025
laurent.mistahl <laurent.mistahl@ff1e8092-12d3-63eb-d73d-7507d0dcd924>
2013-04-12 09:35:28 +0000

    Compilation by using ant instead of tools.jar

--- trunk/saadasvn/java/sources/saadadb/generationclass/Compile.java
+++ trunk/saadasvn/java/sources/saadadb/generationclass/Compile.java
@@ -9,16 +9,63 @@ import org.apache.tools.ant.ProjectHelper;
 
 import saadadb.util.Messenger;
 
+/**
+ * Compile a java class which both source an bin directories are class_mapping
+ * The compilation is done by the ant task located in SAADA_DB_HOME/bin/build.xml
+ * Ant target:
+ * 	<target name="javaclass.compile">
+     	<javac fork="true"  debug="on" 
+		       target="${javac.target}" 
+		       source="${javac.target}" 
+		       srcdir="${SAADA_DB_HOME}/class_mapping" 
+			   destdir="${SAADA_DB_HOME}/class_mapping"
+			   includes="${class.source}">
+            <classpath refid="saadadb.classpath"/>
+		</javac>
+	</target>
+ *
+ * @author michel
+ * @version $Id$
+ *
+ */
 public class Compile{      
   
-    public static void compileItWithAnt(String rootDir, String classToCompile) throws Exception{
-    	compileItWithAnt(rootDir, classToCompile, Messenger.getOutput(), false);
+    /**
+     * Compile the class classToCompile with an output merged to Messenger streams.
+     * Compilation log is minimal.
+     * The saadadbHome is specified is passed as parameter because this method ca be called 
+     * at database creation time
+     * @param saadadbHome
+     * @param classToCompile
+     * @throws Exception
+     */
+    public static void compileItWithAnt(String saadadbHome, String classToCompile) throws Exception{
+    	compileItWithAnt(saadadbHome, classToCompile, Messenger.getOutput(), false);
      }
-    public static void compileItWithAnt(String rootDir, String classToCompile, String logDir) throws Exception{
-    	compileItWithAnt(rootDir, classToCompile,new PrintStream(new FileOutputStream(logDir + File.separator + classToCompile + ".complog")), true);
+    /**
+     * Compile the class classToCompile with an output logged in a filer located in logDir.
+     * Compilation log is talkative. Log file is classToCompile.complog
+     * The saadadbHome is specifier is passed as parameter because this method ca be called 
+     * at database creation time
+     * @param saadadbHome
+     * @param classToCompile
+     * @param logDir
+     * @throws Exception
+     */
+    public static void compileItWithAnt(String saadadbHome, String classToCompile, String logDir) throws Exception{
+    	compileItWithAnt(saadadbHome, classToCompile,new PrintStream(new FileOutputStream(logDir + File.separator + classToCompile + ".complog")), true);
      }
  
-    public static void compileItWithAnt(String rootDir, String classToCompile, PrintStream printStream, boolean debug) throws Exception{
+    /**
+     * Ivoke the ant task javaclass.compile to compile the class classToCompile.
+     * Log messaged are pushed into the printStream. Log are detailed if debug is true
+     * @param saadadbHome
+     * @param classToCompile
+     * @param printStream
+     * @param debug
+     * @throws Exception
+     */
+    private static void compileItWithAnt(String saadadbHome, String classToCompile, PrintStream printStream, boolean debug) throws Exception{
     	Project p = new Project();
         p.setUserProperty("class.source", classToCompile + ".java");
         p.init();
@@ -28,58 +75,9 @@ public class Compile{
 		log.setOutputPrintStream(printStream);
 		log.setMessageOutputLevel((debug)?Project.MSG_VERBOSE: Project.MSG_INFO);
 		p.addBuildListener(log);
-		helper.parse(p, new File(rootDir + File.separator + "bin" + File.separator  + "build.xml"));
+		helper.parse(p, new File(saadadbHome + File.separator + "bin" + File.separator  + "build.xml"));
 		Messenger.printMsg(Messenger.TRACE, "Compile class " + classToCompile);
 		p.executeTarget("javaclass.compile");
     }
-   
-//    public static void main(String[] args) throws Exception {
-//    	Database.init("ThreeXMM");
-//    		Compile.compileItWithAnt(Database.getRoot_dir(), "CUBEUserColl.java");
-//    		Compile.compileItWithAnt(Database.getRoot_dir(), "CUBEUserColl.java", "/tmp/comp.log");
-//    	}
-//
-//	
-//    public static void compileIt(File sourcefile) throws Exception{
-//    	compileIt(sourcefile,Database.getLogDir() );
-//    }
-//   /** * @version $Id$
-//
-//     * @param sourcefile
-//     * @param classdir
-//     * @param log_dir
-//     * @throws Exception
-//     */
-//    public static void compileIt(File sourcefile, String log_dir) throws Exception{
-//    	String classpath = "-classpath";
-//    	String logfile = log_dir + separ +  sourcefile.getName() + ".comp.log";
-//    		PrintWriter out = new PrintWriter( new FileWriter(logfile ) );
-//    		String classname = sourcefile.getName();
-//    		String classdir =  sourcefile.getParent();
-//    		if( classdir.length() > 20 ) 
-//    			if( Messenger.debug_mode ) {
-//    				if( classdir.length() > 20 )  {
-//    					Messenger.printMsg(Messenger.DEBUG, "Compiling: " + classname + " ( -d ..." + classdir.substring(classdir.length() - 20) + ") ");
-//    				}
-//    				else {
-//    	   				Messenger.printMsg(Messenger.DEBUG, "Compiling: " + classname + " ( -d " + classdir + ") "); 					
-//    				}
-//    			}
-//     		
-//    		int status =  Main.compile(new String[]{classpath, System.getProperty("java.class.path"), "-d", classdir, "-target", RunProperties.JVM_TARGET, sourcefile.getAbsolutePath()} ,out);
-//    		out.close();
-//    		if( status == 0 ){
-//    			Messenger.printMsg(Messenger.TRACE, " Compilation of "+classname+".java met no problems"); 
-//    		}else{
-//    			BufferedReader lf;
-//    			lf = new BufferedReader(new FileReader(logfile));
-//    			String str;
-//    			while( (str = lf.readLine()) != null ) {
-//    				Messenger.printMsg(Messenger.ERROR, str.toString());  			
-//    			}
-//    			lf.close();
-//    			FatalException.throwNewException(SaadaException.INTERNAL_ERROR, "compilation failed");
-//    		}
-//     }
 }
   
