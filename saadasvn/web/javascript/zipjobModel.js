c7fb79b61e924cb41c70eddbba0fe4896ec94032
laurent.mistahl <laurent.mistahl@ff1e8092-12d3-63eb-d73d-7507d0dcd924>
2013-05-25 17:22:24 +0000

    style update

--- saadasvn/web/javascript/zipjobModel.js
+++ saadasvn/web/javascript/zipjobModel.js
@@ -66,7 +66,7 @@ jQuery.extend({
 		this.download = function() {
 			if( that.results.length >= 1 ) {
 				var url = that.results[0];
-				downloadLocation(url);
+				Location.changeLocation(url,"Download ZIPBALL");
 			} else {
 				Modalinfo.info("No ZIP archive available");
 			}
