cde81b34c139a1c8c356b1ecf09dc7a81238e9c1
laurent.mistahl <laurent.mistahl@ff1e8092-12d3-63eb-d73d-7507d0dcd924>
2013-09-06 14:39:39 +0000

    Suppression des nome de collectione dans les requetes  ADQL Messages de debug non encore retirés

--- trunk/saadasvn/java/sources/saadadb/resourcetest/ADQLTester.java
+++ trunk/saadasvn/java/sources/saadadb/resourcetest/ADQLTester.java
@@ -1,9 +1,18 @@
 package saadadb.resourcetest;
 
+import java.io.ByteArrayInputStream;
+
+import adqlParser.SaadaADQLQuery;
+import adqlParser.SaadaDBConsistency;
+import adqlParser.SaadaQueryBuilderTools;
+import adqlParser.parser.AdqlParser;
 import saadadb.command.ArgsParser;
 import saadadb.database.Database;
+import saadadb.database.Repository;
 import saadadb.query.result.SaadaQLResultSet;
 import saadadb.vo.ADQLExecutor;
+import saadadb.vo.request.query.AdqlQuery;
+import saadadb.vo.tap.TAPToolBox;
 
 public class ADQLTester {
 
@@ -13,9 +22,25 @@ public class ADQLTester {
 	public static void main(String[] args) throws Exception{
 		ArgsParser ap = new ArgsParser(args);
 		Database.init(ap.getDBName());
-		String query = ap.getQuery();
-		ADQLExecutor executor = new ADQLExecutor();
-		SaadaQLResultSet result = executor.execute(query, -1);
+		String queryStr = ap.getQuery();
+		System.out.println(queryStr);
+//		AdqlParser parse;
+//		SaadaDBConsistency dbConsistency;
+//		dbConsistency = new SaadaDBConsistency();
+//		parse = new AdqlParser(new ByteArrayInputStream(queryStr.getBytes()), null, dbConsistency, new SaadaQueryBuilderTools((SaadaDBConsistency)dbConsistency));
+//		SaadaADQLQuery query = (SaadaADQLQuery)parse.Query();
+//		System.out.println(query.toSQL());
+		//System.exit(1);
+System.out.println("-----------------------------");
+		//
+//		AdqlQuery exec = new AdqlQuery();
+//		exec.
+//		exec.
+		ADQLExecutor executor = new ADQLExecutor();		
+		
+		TAPToolBox.executeTAPQuery(queryStr, true, "votable", 10, "/home/michel/Desktop", "reportNameRoot.xml");
+		System.exit(1);
+		SaadaQLResultSet result = executor.execute(queryStr, -1);
 		int cpt =0;
 		while( result.next()) {
 			if( (cpt % 10) == 0 )
