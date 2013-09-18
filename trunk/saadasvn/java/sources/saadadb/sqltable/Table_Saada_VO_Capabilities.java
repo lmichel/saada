4dc3636afdcee58e727172aa4d78f0e7209dd190
laurent.mistahl <laurent.mistahl@ff1e8092-12d3-63eb-d73d-7507d0dcd924>
2013-09-18 16:14:50 +0000

    datalink column fix

--- trunk/saadasvn/java/sources/saadadb/sqltable/Table_Saada_VO_Capabilities.java
+++ trunk/saadasvn/java/sources/saadadb/sqltable/Table_Saada_VO_Capabilities.java
@@ -191,7 +191,7 @@ public class Table_Saada_VO_Capabilities extends SQLTable {
 				String tbl = Database.getWrapper().getCollectionTableName(dataTreePath.collection, category);
 				//SQLTable.addQueryToTransaction("UPDATE " + tbl + " SET DATALINK = oidsaada" );
 				SQLTable.dropTableColumnIndex(tbl, "DATALINK", null);
-				SQLTable.addQueryToTransaction("UPDATE " + tbl + " SET DATALINK = " + Database.getWrapper().getStrcatOp("'" + Database.getUrl_root() + "/smartdatalink?oid='", "DATALINK" ));
+				SQLTable.addQueryToTransaction("UPDATE " + tbl + " SET DATALINK = " + Database.getWrapper().getStrcatOp("'" + Database.getUrl_root() + "/smartdatalink?oid='", "oidsaada" ));
 			}
 		} catch(FatalException e) {
 			//Category not referenced: likely ObsCore
