907e5fe1b8b20f1c6446b93fd6c565bdf4ae6077
laurent.mistahl <laurent.mistahl@ff1e8092-12d3-63eb-d73d-7507d0dcd924>
2013-11-08 15:32:29 +0000

    Selector for the escape sequence for quotes

--- trunk/saadasvn/java/sources/saadadb/database/PostgresWrapper.java
+++ trunk/saadasvn/java/sources/saadadb/database/PostgresWrapper.java
@@ -123,7 +123,7 @@ public class PostgresWrapper extends DbmsWrapper {
 			Statement stmt = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_UPDATABLE)	;
 			try {
 				QUOTE_SEQUENCE = NEW_QUOTE_SEQUENCE;
-				String q = "SELECT " + getEscapeQuote("aaa'bbb");
+				String q = "SELECT '" + getEscapeQuote("aaa'bbb") + "'";
 				if (Messenger.debug_mode)
 					Messenger.printMsg(Messenger.DEBUG, "Try " + q);
 				stmt.execute(q);
@@ -133,7 +133,7 @@ public class PostgresWrapper extends DbmsWrapper {
 			} catch(Exception e) {
 				try {
 					QUOTE_SEQUENCE = OLD_QUOTE_SEQUENCE;
-					String q = "SELECT " + getEscapeQuote("'aaa'bbb'");
+					String q = "SELECT '" + getEscapeQuote("aaa'bbb")+ "'";
 					if (Messenger.debug_mode)
 						Messenger.printMsg(Messenger.DEBUG, "Try " + q);
 					stmt.execute("SELECT " + getEscapeQuote("'aaa'bbb'"));
