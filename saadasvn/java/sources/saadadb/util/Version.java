a03fe15c8d91229c706cc76fe4fb8f72aebac5e4
laurent.mistahl <laurent.mistahl@ff1e8092-12d3-63eb-d73d-7507d0dcd924>
2013-11-18 10:33:17 +0000

    Build8

--- saadasvn/java/sources/saadadb/util/Version.java
+++ saadasvn/java/sources/saadadb/util/Version.java
@@ -7,7 +7,7 @@ package saadadb.util;
  */
 final public class Version {
 	private static final String version = "1.7.0";
-	private static final String build = "7";
+	private static final String build = "8";
 	
 	public static String getVersion() {
 		return version + ".build" + build;
