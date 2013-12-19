b4428aab617b22c6186e06cbc48df3abda05f6c0
laurent.mistahl <laurent.mistahl@ff1e8092-12d3-63eb-d73d-7507d0dcd924>
2013-12-19 16:12:14 +0000

    Polygone processor (dev by R.Julien)

---
+++ trunk/saadasvn/java/sources/saadadb/query/region/test/gridcreator/UpdateHPXDirectSQL.java
@@ -0,0 +1,65 @@
+package saadadb.query.region.test.gridcreator;
+
+import healpix.core.HealpixIndex;
+import healpix.tools.SpatialVector;
+
+import java.sql.ResultSet;
+
+import saadadb.database.Database;
+import saadadb.sqltable.SQLQuery;
+import saadadb.sqltable.SQLTable;
+
+/**
+ * Class UpdateHPXDirectSQL
+ * This class allows to update the healpix column directly in the Database 
+ * @author jremy
+ * @version $Id$
+ *
+ */
+public class UpdateHPXDirectSQL {
+	
+	public static final String tableName="collection";
+	
+	public static final String bddName="base3reso3";
+
+	public static void main(String[] args) throws Exception {
+		long tpsdepart=System.currentTimeMillis();
+		Database.init(bddName);
+		SQLQuery sqlq = new SQLQuery();
+		String query = "SELECT pos_ra_csa,pos_dec_csa FROM "+tableName+";";
+		ResultSet rs = sqlq.run(query);
+
+		SQLTable.beginTransaction();
+		while (rs.next()){
+			int ligne=rs.getRow();
+			double ra=rs.getDouble("pos_ra_csa");
+			double dec=rs.getDouble("pos_dec_csa");
+			SpatialVector sv = new SpatialVector(ra,dec);
+			HealpixIndex hi8 = new HealpixIndex(256);
+			HealpixIndex hi10 = new HealpixIndex(1024);
+			HealpixIndex hi12 = new HealpixIndex(4096);
+			long pix8=hi8.vec2pix_nest(sv);
+			long pix10=hi10.vec2pix_nest(sv);
+			long pix12=hi12.vec2pix_nest(sv);
+
+			
+			String querypix = "UPDATE "+tableName+" SET healpix8 ="+pix8+", healpix10 ="+pix10+", healpix12 ="+pix12+" WHERE rowid="+ligne+";";
+
+			
+			SQLTable.addQueryToTransaction(querypix);
+			
+			
+			
+		}
+		
+		SQLTable.commitTransaction();
+		
+		long tpsarrivee=System.currentTimeMillis();
+		
+		long tpstotal=tpsarrivee-tpsdepart;
+		
+		System.out.println(TpsExec.getTime(tpstotal));
+		
+	}
+
+}
---
+++ trunk/saadasvn/java/sources/saadadb/query/region/triangule/Triangleur.java
@@ -0,0 +1,114 @@
+package saadadb.query.region.triangule;
+
+import java.util.ArrayList;
+
+import saadadb.query.region.triangule.convertCoord.radecToXY;
+import cds.aladin.Aladin;
+
+/**
+ * Class calling the converter to triangule the polygon
+ * This class can also call Aladin to show the polygon or the triangles
+ * @author jremy
+ * @version $Id$
+ *
+ */
+public class Triangleur {
+
+	/**
+	 * Atribute points representing the List of points of the polygon
+	 */
+	private ArrayList<Point> points;
+
+	/**
+	 * Attribute triangle representing the list of triangles returned by the Triangulator
+	 */
+	private ArrayList<Triangle> triangles;
+
+	/**
+	 * Constructor Triangleur from a list of points
+	 * @param points : ArrayList<Point>
+	 */
+	public Triangleur (ArrayList<Point> points) {
+		this.points=points;
+		triangles = new ArrayList<Triangle>();
+	}
+
+	/**
+	 * Constructor Triangleur from a polygone
+	 * @param p : Polygon
+	 */
+	public Triangleur (Polygone p) throws Exception{
+		this.points=p.points;
+		triangles = new ArrayList<Triangle>();
+		radecToXY convert = new radecToXY(points);
+		triangles=convert.process();
+	}
+
+	/**
+	 * This method allows to get the list of Triangles relevant for Aladin
+	 * @return ArrayList<String>
+	 */
+	public ArrayList<String> getTriangleAladin() {
+		ArrayList<String> trianglesAladin = new ArrayList<String>();
+		for (Triangle t : this.triangles) {
+			trianglesAladin.add(t.getAladin());
+		}
+		return trianglesAladin;
+	}
+
+	/**
+	 * This method allows to get the String associate with the polygon relevant for Aladin
+	 * @return String
+	 */
+	public String getPolygonAladin () {
+		String ret = "draw blue polygon ";
+		for (Point p : this.points) {
+			ret+=" "+p.getRa()+","+p.getDec();
+		}
+		return ret;
+	}
+
+	/**
+	 * This method allows to launch Aladin, set the sky and draw the polygon and the triangles
+	 * @param onlyPolygon : boolean at true if you only need the draw of the polygon
+	 * @throws Exception
+	 */
+	public void startAladin(boolean onlyPolygon) throws Exception{
+		Aladin aladin = Aladin.launch("-trace");
+		aladin.execCommand("get allsky(\"DSS colored\")");
+		aladin.execCommand(this.getPolygonAladin());
+		if (!onlyPolygon) {
+			for (String s : this.getTriangleAladin()) {
+				aladin.execCommand(s);
+			}
+		}
+	}
+	
+	/**
+	 * This method allows to impress the gnuplot commands for the polygon and the triangles
+	 * @param onlyPolygon : boolean at true if you only need the draw of the polygon
+	 * @throws Exception
+	 */
+	public void getGnuplot(boolean onlyPolygon) throws Exception {
+		Polygone p = new Polygone (points);
+		System.out.println(p.getPolyGnuplot());
+		if (!onlyPolygon) {
+			int iterator=2;
+			for (Triangle t : this.triangles) {
+				System.out.println("set object "+iterator+t.getTriangleGnuplot());
+				iterator++;
+			}
+		}
+	}
+
+	/**
+	 * This method allows to convert from radec to xy and then return the triangles
+	 * @return ArrayList<Triangle>
+	 * @throws Exception
+	 */
+	public ArrayList<Triangle> execute() throws Exception{
+		radecToXY convert = new radecToXY(this.points);		
+		return convert.process();		
+	}
+	
+}
