839e198be21b45421edb07f7c6f1670a7a897968
petrol.stud@gmail.com <petrol.stud@gmail.com@ff1e8092-12d3-63eb-d73d-7507d0dcd924>
2014-09-23 07:01:37 +0000

    Created new package and moved package enum in it

---
+++ branches/obscore/java/sources/saadadb/vocabulary/enums/DataFileExtensionType.java
@@ -0,0 +1,17 @@
+package saadadb.vocabulary.enums;
+
+/**
+ * Allowed types FITS extension
+ * BINTABLE or ASCIITABLE cab also be used for VOTables
+ * @author michel
+ * @version $Id$
+ */
+public enum DataFileExtensionType {
+	BASIC,
+	TILE_COMPRESSED_IMAGE,
+	BINTABLE,
+	ASCIITABLE,
+	TABLE_COLUMNS,
+	IMAGE,
+	UNSUPPORTED
+}
---
+++ branches/obscore/java/sources/saadadb/vocabulary/enums/DispersionType.java
@@ -0,0 +1,14 @@
+package saadadb.vocabulary.enums;
+
+
+/**
+ * Dispersion type supported 
+ * @author michel
+ * @version $Id$
+ */
+public enum DispersionType {
+	WAVELENGTH,
+	FREQUENCY ,
+	ENERGY,
+	CHANNEL
+}
