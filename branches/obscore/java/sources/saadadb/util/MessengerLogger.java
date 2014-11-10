467bb3bda1a229547c3ae4f4e07b07f6c0c15ef8
laurent.mistahl <laurent.mistahl@ff1e8092-12d3-63eb-d73d-7507d0dcd924>
2014-11-10 08:39:47 +0000

    Wrapper for compat with hecds lib

---
+++ branches/obscore/java/sources/saadadb/util/MessengerLogger.java
@@ -0,0 +1,38 @@
+
+package saadadb.util;
+
+import hecds.Logger;
+
+/**
+ * Decorator of the Messenger t be used by the HECDS package logger
+ * @author michel
+ * @version $Id$
+ */
+public class MessengerLogger implements Logger {
+
+	@Override
+	public void trace(String msg) {
+		Messenger.printMsg(Messenger.TRACE, msg);
+	}
+
+	@Override
+	public void debug(String msg) {
+		Messenger.printMsg(Messenger.DEBUG, msg);
+	}
+
+	@Override
+	public void error(String msg) {
+		Messenger.printMsg(Messenger.ERROR, msg);
+	}
+
+	@Override
+	public void warn(String msg) {
+		Messenger.printMsg(Messenger.WARNING, msg);
+	}
+
+	@Override
+	public void locatedMsg(String msg) {
+		Messenger.printLocatedMsg(msg.toString());
+	}
+
+}
