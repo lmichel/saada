89e683464ebe5971a677fdb2566bd735e1188fe7
Laurent MICHEL <laurent.michel@astro.unistra.fr>
2015-04-20 13:05:19 +0200

    SQLIte spooler failure when Tomcat reload the Webapp

--- saadasvn/java/sources/saadadb/util/Version.java
+++ saadasvn/java/sources/saadadb/util/Version.java
@@ -7,7 +7,7 @@ package saadadb.util;
  */
 final public class Version {
 	private static final String version = "1.8";
-	private static final String build = "10";
+	private static final String build = "11";
 	
 	public static String getVersion() {
 		return version + ".build" + build;
