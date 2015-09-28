9531dac47898f7fcfe3356476356f382842d95ab
Laurent MICHEL <laurent.michel@astro.unistra.fr>
2015-09-28 11:40:14 +0200

    build14

--- saadasvn/java/sources/saadadb/util/Version.java
+++ saadasvn/java/sources/saadadb/util/Version.java
@@ -7,7 +7,7 @@ package saadadb.util;
  */
 final public class Version {
 	private static final String version = "1.8";
-	private static final String build = "13";
+	private static final String build = "14";
 	
 	public static String getVersion() {
 		return version + ".build" + build;
