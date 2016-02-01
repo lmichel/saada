c5a072cf4cf6737ef0ad64f0ae02e4086718a155
Laurent MICHEL <laurent.michel@astro.unistra.fr>
2016-02-01 13:40:46 +0100

    version number

--- java/sources/saadadb/util/Version.java
+++ java/sources/saadadb/util/Version.java
@@ -7,7 +7,7 @@ package saadadb.util;
  */
 final public class Version {
 	private static final String version = "2.0";
-	private static final String build = "18";
+	private static final String build = "17";
 	public static final String date= "01/02/16 10:00";
 	
 	public static String getVersion() {
