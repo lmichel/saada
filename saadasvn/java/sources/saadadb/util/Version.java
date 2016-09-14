fb744bb94f86c2e7ba25bb5b121206a2fd6152b1
Laurent Michel <laurent.michel@astro.unistra.fr>
2016-09-14 13:48:33 +0200

    version increment

--- saadasvn/java/sources/saadadb/util/Version.java
+++ saadasvn/java/sources/saadadb/util/Version.java
@@ -7,7 +7,7 @@ package saadadb.util;
  */
 final public class Version {
 	private static final String version = "1.9";
-	private static final String build = "2";
+	private static final String build = "3";
 	
 	public static String getVersion() {
 		return version + ".build" + build;
