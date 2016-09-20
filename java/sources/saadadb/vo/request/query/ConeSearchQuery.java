044ffd23885d0baad574e4bcf9c14808d65a6610
Laurent Michel <laurent.michel@astro.unistra.fr>
2016-09-20 14:21:50 +0200

    size limited to 10deg

--- java/sources/saadadb/vo/request/query/ConeSearchQuery.java
+++ java/sources/saadadb/vo/request/query/ConeSearchQuery.java
@@ -144,6 +144,8 @@ public class ConeSearchQuery extends VOQuery {
 		}
 		if ( size < 0.0) {
 			QueryException.throwNewException("ERROR","SIZE must be greater than 0.0 ");
+		} else	if( size > 10 ){
+			QueryException.throwNewException("ERROR","SIZE is limited to 10 deg");
 		}					
 		protocolParams.put("size", value);
 		size *= 60;
