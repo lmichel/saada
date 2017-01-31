2ee3673ac150ad93ffbf692a48783a4a7ac7363f
Laurent Michel <laurent.michel@astro.unistra.fr>
2017-01-31 17:49:42 +0100

    Web interface

--- java/sources/saadadb/util/Version.java
+++ java/sources/saadadb/util/Version.java
@@ -7,8 +7,8 @@ package saadadb.util;
  */
 final public class Version {
 	private static final String version = "2.0";
-	private static final String build = "29";
-	public static final String date= "27/01/17 10:00"; 
+	private static final String build = "30";
+	public static final String date= "31/01/17 10:00"; 
 	
 	public static String getVersion() {
 		return version + ".build" + build;
