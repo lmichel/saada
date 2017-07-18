9916bbb7d3e5c0a2966f3ba90720c248d89945c7
Laurent Michel <laurent.michel@astro.unistra.fr>
2017-07-18 18:29:46 +0200

    Test class for position lists with errors

---
+++ Saada1.7/java/sources/saadadb/resourcetest/PosListWithError.java
@@ -0,0 +1,22 @@
+package saadadb.resourcetest;
+
+import saadadb.command.ArgsParser;
+import saadadb.database.Database;
+import saadadb.database.spooler.Spooler;
+import saadadb.exceptions.FatalException;
+import saadadb.exceptions.QueryException;
+import saadadb.query.executor.Query;
+import saadadb.query.result.SaadaQLResultSet;
+
+public class PosListWithError {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		ArgsParser ap = new ArgsParser(args);
+		Database.init(ap.getDBName());
+		Query q = new Query();
+		SaadaQLResultSet srs = q.runQuery("Select ENTRY From MergedEntry In MERGEDCATALOGUE \n WherePosition{isInCircle(\"poslist:/home/michel/Desktop/list.pos\",1.0 ,J2000,FK5)}\n");
+		Spooler.getSpooler().close();
+	}
+
+}
